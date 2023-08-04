427546e2d00a3840dff089e5a263aa0da39e8d95
mbarber <mbarber@ne47.cbio>
2023-08-04 08:30:09 +0000

    updated requirements

--- openprotein/_version.py
+++ openprotein/_version.py
@@ -1,3 +1,6 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version # py 3.7
 
 __version__ = version("openprotein-python")
